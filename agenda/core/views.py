ffabd00cf4fb273eca1b7b8df55a0eb89843c692
Giuliano Richards <giulianor2@gmail.com>
2022-11-08 21:58:29 -0300

    Add files via upload
    HTML

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from core.models import Evento
+
 
 # Create your views here.
+
+def lista_eventos(request):
+    #evento = Evento.objects.get(id=1)
+    #esponse = {'evento': evento}
+    #return render(request, 'agenda.html', response)
+
+    # pegar o usuario que fez a requisição (proximas duas linhas)
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    #evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
